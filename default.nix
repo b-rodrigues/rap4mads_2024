8d3dc653ca8e8020d8b84c508d59cd049cb08a44
Bruno Rodrigues <bruno@brodrigues.co>
2024-12-02 14:41:43 +0100

    merge conflict

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e1b70fb06158edd5dde073031fb367b6e709cf3f.tar.gz") {};
  rpkgs = builtins.attrValues {
-  inherit (pkgs.rPackages) quarto Ecdat devtools janitor plm pwt9 rio targets tarchetypes testthat tidyverse usethis formatR;
+  inherit (pkgs.rPackages) quarto Ecdat devtools janitor plm pwt9 rio targets tarchetypes testthat tidyverse usethis formatR renv;
 };
   tex = (pkgs.texlive.combine {
   inherit (pkgs.texlive) scheme-small amsmath framed fvextra environ fontawesome5 orcidlink pdfcol tcolorbox tikzfill;
@@ -9,6 +9,24 @@ let
  system_packages = builtins.attrValues {
   inherit (pkgs) R glibcLocalesUtf8 quarto;
 };
+
+  myPackage = [(pkgs.rPackages.buildRPackage {
+  name = "myPackage";
+  src = pkgs.fetchgit {
+    url = "https://github.com/b-rodrigues/myPackage";
+    branchName = "master";
+    rev = "e9d9129de3047c1ecce26d09dff429ec078d4dae";
+    sha256 = "sha256-u/tIo9L+S12ssGNlQu7AVHG5W5OTpXZ+rHn1Pz6RIKs=";
+    };
+propagatedBuildInputs = builtins.attrValues {
+        inherit (pkgs.rPackages) 
+          dplyr
+          janitor
+          rlang;
+      };
+  }) 
+  ];
+
   in
   pkgs.mkShell {
     LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
@@ -19,6 +37,6 @@ let
     LC_PAPER = "en_US.UTF-8";
     LC_MEASUREMENT = "en_US.UTF-8";
 
-    buildInputs = [  rpkgs tex system_packages  ];
+    buildInputs = [ rpkgs tex system_packages myPackage ];
       
   }
